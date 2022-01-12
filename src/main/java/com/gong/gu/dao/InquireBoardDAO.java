c5ea3db15560bcce4bcd47beac2672e18c44f58a
kim hyun woo <khw717140@gmail.com>
2022-01-12 17:58:35 +0900

    2022.01.12일 17시57분 (inqwriteForm, inqupdateForm, inquireBoardService, inquireBoardDAO, inquireBoardMapper)

--- src/main/java/com/gong/gu/dao/InquireBoardDAO.java
+++ src/main/java/com/gong/gu/dao/InquireBoardDAO.java
@@ -1,9 +1,19 @@
 package com.gong.gu.dao;
 
+import java.util.HashMap;
+
 import com.gong.gu.dto.BoardDTO;
 
 public interface InquireBoardDAO {
 
 	void inqwrite(BoardDTO dto);
+	
+	void fileWrite(int idx, String oriFileName, String newFileName);
+
+	int inqwrite2(int idx, String category);
+
+	int update(HashMap<String, String> params);
+	
+	
 
 }
--- src/main/java/com/gong/gu/service/InquireBoardService.java
+++ src/main/java/com/gong/gu/service/InquireBoardService.java
@@ -14,6 +14,8 @@ import com.gong.gu.dto.BoardDTO;
 @Service
 public class InquireBoardService {
 	@Autowired InquireBoardDAO dao;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	// 문의글쓰기
 	public String inqwrite(MultipartFile[] photos, HashMap<String, String> params) {
@@ -21,28 +23,82 @@ public class InquireBoardService {
 		String page = "redirect:/";
 		
 		BoardDTO dto = new BoardDTO();
-		
-		dto.setBoard_title("board_title");
-		dto.setBoard_content("board_content");
-		
+		dto.setBoard_title(params.get("board_title"));
+		dto.setBoard_content(params.get("board_content"));
 		dao.inqwrite(dto);
 		
 		int idx = dto.getBoard_no();
-		
+		logger.info("idx: "+idx);
 		
 		if (idx>0) {
-			page = "redirect:/detail?idx="+idx;
+			page = "readirect:/detail?idx="+idx;
+			inqwrite2(idx,params);
+			saveFile(idx,photos);
 		}
-		
-		
+
 		return page;
 	}
 	
+	
+	
+	
+	private void inqwrite2(int idx, HashMap<String, String> params) {
+		logger.info("옵션 DB 데이터 넣기 : {}",params);
+		
+		try {
+			String Category = params.get("Category");
+			
+			int row = dao.inqwrite2(idx,Category);
+			logger.info("입력된 건수 : {}",row);
+			
+		} catch (Exception e) {
+
+		}
+		
+	}
+
+
+
+
+	private void saveFile(int idx, MultipartFile[] photos) {
+		for (MultipartFile photo : photos) {
+			
+			try {
+				String oriFileName = photo.getOriginalFilename();
+				int index = oriFileName.lastIndexOf(".");
+				logger.info("index : {}",index);
+				
+				if (index>0) {
+					String ext = oriFileName.substring(index);
+					String newFileName = System.currentTimeMillis()+ext;
+					logger.info(oriFileName+ "=>" +newFileName);
+				}
+				
+				
+				
+			} catch (Exception e) {
+
+			}
+		}
+	}
+	
+
+
+
 
 	// 문의글 수정
 	public String inqupdate(MultipartFile[] photos, HashMap<String, String> params) {
 		
-		return null;
+		int idx = Integer.parseInt(params.get("idx"));
+		String page = "redirect:/detail?idx="+idx;
+		
+		if (dao.update(params)>0) {
+			page = "redirect:/detail?idx="+idx;
+			saveFile(idx, photos);
+		}
+		
+		
+		return page;
 	}
 
 
